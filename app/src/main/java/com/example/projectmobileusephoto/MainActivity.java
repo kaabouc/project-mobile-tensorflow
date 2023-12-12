ca3e0cbcb062f3017f55c18124985c378505f09c
kaabouc <kaabouchmohamed13@gmail.com>
2023-12-12 10:12:00 +0000

    apres fonction de model

--- app/src/main/java/com/example/projectmobileusephoto/MainActivity.java
+++ app/src/main/java/com/example/projectmobileusephoto/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.projectmobileusephoto;
 
 import androidx.annotation.NonNull;
-import android.Manifest;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,143 +17,154 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.projectmobileusephoto.ml.MobilenetV110224Quant;
+import com.example.projectmobileusephoto.ml.Model1;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
-Button selectBtn , predicBtn , captureBtn;
-Bitmap bitmap;
-TextView result;
-ImageView imageView;
-
+    Button select,predict,capture;
+    TextView result;
+    Bitmap bitmap;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getPermission();
-//        String[] labels = new String[0];
-//        int cnt = 0 ;
-//        try {
-//            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(getAssets().open("labels.txt")));
-//            String line= bufferedReader.readLine();
-//            while (line!=null){
-//                labels[cnt]= line ;
-//                cnt++;
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+       /* String[] labels=new String[1001];
+        int cmt=0;
+        try {
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(getAssets().open("Database")));
+            String line=bufferedReader.readLine();
+            while (line!=null){
+                labels[cmt]=line;
+                cmt++;
+                line=bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+        select=findViewById(R.id.selectBtn);
+        predict=findViewById(R.id.predicBtn);
+        capture=findViewById(R.id.captureBtn);
+        result=findViewById(R.id.result);
+        imageView=findViewById(R.id.imageView);
 
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,10);
+            }
+        });
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,12);
+            }
+        });
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Model1 model = Model1.newInstance(MainActivity.this);
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+                    bitmap=Bitmap.createScaledBitmap(bitmap,224,224,true);
+                    //inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
+                    TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+                    tensorImage.load(bitmap);
+                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                    inputFeature0.loadBuffer(byteBuffer);
+                    Model1.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+                    result.setText(getMax(outputFeature0.getFloatArray())+" ");
+                    model.close();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+       /* predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    MobilenetV110224Quant model = MobilenetV110224Quant.newInstance(MainActivity.this);
 
-        selectBtn = findViewById(R.id.selectBtn);
-        predicBtn = findViewById(R.id.predicBtn);
-        captureBtn = findViewById(R.id.captureBtn);
-        imageView= findViewById(R.id.imageView);
-       result = findViewById(R.id.result);
-       selectBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent intent = new Intent();
-               intent.setAction(Intent.ACTION_GET_CONTENT);
-               intent.setType("image/*");
-               startActivityForResult(intent, 10);
-           }
-       });
-       captureBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-               startActivityForResult(intent,12);
-           }
-       });
-       predicBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               try {
-                   MobilenetV110224Quant model = MobilenetV110224Quant.newInstance(MainActivity.this);
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
+                    bitmap=Bitmap.createScaledBitmap(bitmap,224,224,true);
+                    inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer() );
 
-                   // Creates inputs for reference.
-                   TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
-                   bitmap = Bitmap.createScaledBitmap(bitmap,224,224,true);
+                    // Runs model inference and gets result.
+                    MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
 
-                   inputFeature0.loadBuffer(TensorImage.fromBitmap(bitmap).getBuffer());
-
-                   // Runs model inference and gets result.
-                   MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
-                   TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-                    result.setText(getMax(outputFeature0.getFloatArray())+"");
-                   // Releases model resources if no longer used.
-                   model.close();
-               } catch (IOException e) {
-                   // TODO Handle the exception
-               }
-
-           }
-       });
-
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+                    result.setText(labels[getMax(outputFeature0.getFloatArray())]+" ");
+                    // Releases model resources if no longer used.
+                    model.close();
+                } catch (IOException e) {
+                    // TODO Handle the exception
+                }
+            }
+        });*/
     }
-
-    int getMax(float[] array ){
-        int max= 0 ;
-        for (int i =0 ; i<array.length;i++){
-            if (array[i]>array[max]){
-                max = i ;
+    int getMax(float[] arr){
+        int max=0;
+        for(int i=0; i<arr.length;i++){
+            if (arr[i]>arr[max]) max=i;
+        }return max;
+    }
+    void getPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},11);
             }
         }
-        return max;
     }
 
-    private static final int REQUEST_CAMERA_PERMISSION = 100;
-
-// ... (other code in your activity)
-
-    // Request camera permissions if not granted
-    void getPermission() {
-    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 11);
-        }
-       }
-    }
-
-
-
-    // Handle the result of the permission request
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == 11) {
-            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                this.getPermission();
+        if (requestCode==11){
+            if (grantResults.length>0){
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    this.getPermission();
+                }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if ( requestCode==10){
-            Uri uri=data.getData();
-            try{
-                bitmap= MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-               imageView.setImageBitmap(bitmap);
-            }catch (IOException e){
-                e.printStackTrace();
+        if (requestCode == 10) {
+            if (data != null) {
+                Uri uri = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
-        } else if (requestCode==12) {
+
+        } else if (requestCode == 12) {
             bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode,data);
     }
 }
